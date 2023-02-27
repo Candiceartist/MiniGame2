dfa37036771b3f8cc66d2ee1075215fe5014daad
Candice Jovon Juarez <candice.artist@gmail.com>
2023-02-27 14:41:41 -0500

    adding functions to buttons

--- app.js
+++ app.js
@@ -1,87 +1,88 @@
+// player1 fires and deducts one point from alien hull 
+// alien couters immediately after based on accuracy
+// if hit deducts on from player one hull
 
-// 
-// if player 1 hull equal to zero game over else if continue
-// if firepower greater than alien fire power alien hull  minus 1 elese if player1 minus 1
-//  if player one accuracy > than alien accuracy minus 1 you hit alien else if you took a hit player 1 minus1
-//  else if aliens array.length === zero ,"you win"
-// else Draw
-// 
-//  
 
 function getRandomNum1(min, max) {
-    return Math.floor(Math.random() * (max - min +1)) + min;
+  return Math.floor(Math.random() * (max - min +1)) + min;
 }
 function getRandomNum2(min, max) {
-    return Math.floor(Math.random() * (max - min +1) + min)/10;
+  return Math.floor(Math.random() * (max - min +1) + min)/10;
 }
 
 const player1 = {
-    hull: 20,
-    firepower: 5,
-    accuracy: getRandomNum2(6, 8),
+  hull: 20,
+  firepower: 5,
+  accuracy: getRandomNum2(6, 8),
 }
 
-const alien = 
+const alien = [ 
+{hull: getRandomNum1(3, 6),
+  firepower:getRandomNum1(2, 4),
+  accuracy: getRandomNum2(6, 8),
+  },
+  
+  { hull: getRandomNum1(3, 6),
+      firepower: getRandomNum1(2, 4),
+      acurracy: getRandomNum2(6, 8),
+ },
+
+ {hull: getRandomNum1(3, 6),
+  firepower: getRandomNum1(2, 4),
+  acurracy:getRandomNum2(6, 8),
+  },
+  
   {hull: getRandomNum1(3, 6),
-    firepower:getRandomNum1(2, 4),
-    accuracy: getRandomNum2(6, 8),
-    }
+      firepower: getRandomNum1(2, 4),
+      acurracy: getRandomNum2(6, 8),
+  },
 
- const attackTarget = ()=> {
-    if (player1.accuracy < alien.accuracy) {
-        console.log('You have been hit!')
-    } else {
-        console.log("Target hit successfully")
-    }
-}
+ {hull: getRandomNum1(3, 6),
+  firepower: getRandomNum1(2, 4),
+  acurracy: getRandomNum2(6, 8),
+  },
 
-const takeFire = () =>{
-   let health = player1.hull --;
-   console.log(health, "You are under attack")
-   if(health ===0){
-    console.log("Game Over")
+ {hull: getRandomNum1(3, 6),
+ firepower: getRandomNum1(2, 4),
+ acurracy: getRandomNum2(6, 8),
+ },
+]
+
+
+const alienFire = ()=> {
+  player1.hull -= alien[0].firepower;
+  console.log(player1.hull, "You've been Hit!")
+  if(player1.hull <= 6){
+    console.log("Your ship is in critical danger")
    }
+  if(player1.hull <= 0){
+   console.log("Game Over")
+  }
 }
 
-const targetHit = () =>{
-    let health2 = alien.hull --;
-    console.log(health2, "The cretaures are damaged")
-    if(health2 ===0){
-     console.log("You won! The war is over")
-    }
- }
-
-//    { hull: getRandomNum1(3, 6),
-//         firepower: getRandomNum1(2, 4),
-//         acurracy: getRandomNum2(6, 8),
-//    },
-//    {hull: getRandomNum1(3, 6),
-//     firepower: getRandomNum1(2, 4),
-//     acurracy:getRandomNum2(6, 8),
-//     },
-    
-//     {hull: getRandomNum1(3, 6),
-//         firepower: getRandomNum1(2, 4),
-//         acurracy: getRandomNum2(6, 8),
-//     },
-//    {hull: getRandomNum1(3, 6),
-//     firepower: getRandomNum1(2, 4),
-//     acurracy: getRandomNum2(6, 8),
-//     },
-//    {hull: getRandomNum1(3, 6),
-//    firepower: getRandomNum1(2, 4),
-//    acurracy: getRandomNum2(6, 8),
-//    },
-// ]
-
-// if player 1 hull equal to zero game over else if continue
+const retreat = ()=> {
+  if(player1.hull >= 7) {
+   console.log("Don't Give UP! You still have ammo left!")
+  } else {
+    console.log("Game Over")
+  }
+}
 
 
-    //  if (player1.hull === 0) {
-    //  console.log("Game Over")
-    //  } else if ( player1.firepower > alien.firepower) alien.hull--){
-
-    //  }
-
-
+const playerFires = () =>{
+  alien[0].hull -= player1.firepower;
+  console.log(alien[0].hull, "The cretaures are damaged")
+  let playerText = document.querySelector('.story')
+  playerText.innerHTML = "'The cretaures are damaged'"
+  if(alien[0].hull <= 0){
+   console.log("Alien Ship defeated")
+  }
+  if(alien[0].hull <= 0){
+    alien.shift()
+  }
+  if(alien.length ===0 ) {
+    console.log("You Win! The war is over!")
+  }
+  
+}
 
